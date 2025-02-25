63a530d2edd84074e06b67525ec1be7ea3abad1e
Mihir Desai <mihir@mihirdesai.net>
2025-02-25 21:54:43 +0530

    Reset

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://vilyefpc.github.io",
+  integrations: [sitemap()],
+});
