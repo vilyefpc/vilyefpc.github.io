923600408fba3b520ea4c25263197112099031e9
Mihir Desai <mihir@mihirdesai.net>
2025-02-26 22:41:52 +0530

    Setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vilyefpc.github.io",
   integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
