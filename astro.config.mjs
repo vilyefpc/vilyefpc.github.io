4a2fa7765084388322d684e156142b95112ccb44
Mihir Desai <mihir@mihirdesai.net>
2025-04-05 20:07:33 +0530

    Setup tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://vilyefpc.com',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
